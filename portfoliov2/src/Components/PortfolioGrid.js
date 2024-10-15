0a745237e9b1ef87e7f45a4a486814e30bd0a86b
DaneS25 <dane.tyler.sorensen@hotmail.com>
2024-10-15 16:49:18 +1300

    added some placeholder text and logos

--- portfoliov2/src/Components/PortfolioGrid.js
+++ portfoliov2/src/Components/PortfolioGrid.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './PortfolioGrid.css';
+import GitHubIcon from '../Assets/github.jpg'
+import LinkedInIcon from '../Assets/linkedin.jpg'
+import InstagramIcon from '../Assets/instagram.png'
 
 const PortfolioGrid = () => {
     return (
         <div className="portfolio-grid">
-            <div className="grid-item item1">Item 1</div>
-            <div className="grid-item item2">Item 2</div>
+            <div className="grid-item item1">
+                <p className='para1'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit, ligula at tincidunt tristique, nunc nulla hendrerit est, eget vehicula urna lacus nec elit. Donec vel ante sit amet dolor interdum tempus. Praesent scelerisque nisi ut tortor egestas, ut pharetra eros ultrices. Aenean sit amet malesuada lorem. Etiam ultricies urna vitae lectus convallis, nec dignissim tortor fermentum. Phasellus id risus ut mi vehicula auctor. Suspendisse id orci nec ipsum suscipit vulputate non nec tortor. Proin consectetur, velit a facilisis placerat, libero purus ullamcorper purus, in finibus felis mi et lacus.
+                </p>
+            </div>
+            <div className="grid-item item2">
+                <div className="image-container">
+                    <img src={GitHubIcon} alt="Github Icon" className="grid-image" />
+                    <img src={LinkedInIcon} alt="Linkedin Icon" className='grid-image'/>
+                    <img src={InstagramIcon} alt="Instagram Icon" className='grid-image'/>
+                </div>
+            </div>
             <div className="grid-item item3">Item 3</div>
             <div className="grid-item item4">Item 4</div>
-            <div className="grid-item item5">Item 5</div>
+            <div className="grid-item item5"><p className='para1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit, ligula at tincidunt tristique, nunc nulla hendrerit est, eget vehicula urna lacus nec elit. Donec vel ante sit amet dolor interdum tempus. Praesent scelerisque nisi ut tortor egestas, ut pharetra eros ultrices. Aenean sit amet malesuada lorem. Etiam ultricies urna vitae lectus convallis, nec dignissim tortor fermentum. Phasellus id risus ut mi vehicula auctor. Suspendisse id orci nec ipsum suscipit vulputate non nec tortor. Proin consectetur, velit a facilisis placerat, libero purus ullamcorper purus, in finibus felis mi et lacus.</p></div>
             <div className="grid-item item6">Item 6</div>
             <div className="grid-item item7">Item 7</div>
             <div className="grid-item item8">Item 8</div>
